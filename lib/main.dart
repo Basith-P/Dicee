5c786ad9107e20aa1ed197c05862401d7eaf5057
Basith P <basithpofficial@gmail.com>
2021-07-12 18:27:58 +0530

    add images

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Dicee'),
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.blueGrey[900],
+          appBar: AppBar(
+            backgroundColor: Colors.blueGrey[900],
+            title: Text('Dicee'),
+          ),
+          body: Center(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Image.asset('assets/images/dice1.png'),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Image.asset('assets/images/dice2.png'),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
